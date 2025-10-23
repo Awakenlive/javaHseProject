451d0b2b0a4176711cf6cd6b51536ba943a8354a
Awakenlive <110718679+Awakenlive@users.noreply.github.com>
2025-10-23 13:16:06 +0300

    add archeticture, create entities

---
+++ src/domain/entites/priority.java
@@ -0,0 +1,20 @@
+package domain.entites;
+
+public enum priority {
+    LOW("Низкий"),
+    MEDIUM("Средний"),
+    HIGH("Высокий");
+
+    private String displayStatement;
+    priority(String displayStatement){
+        this.displayStatement = displayStatement;
+    }
+
+    public String getDisplayStatement() {
+        return displayStatement;
+    }
+
+    public void setDisplayStatement(String displayStatement){
+        this.displayStatement = displayStatement;
+    }
+}
---
+++ src/domain/entites/task.java
@@ -0,0 +1,41 @@
+package domain.entites;
+
+import java.time.LocalDate;
+
+public class task {
+    private final long id;
+    private String title;
+    private String description;
+    private LocalDate date;
+    private priority priorityTask;
+    private boolean completed;
+
+    task(long id, String title, String description, LocalDate date, priority priorityTask, boolean completed){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priorityTask = priorityTask;
+        this.completed = completed;
+    }
+
+
+    public long getId() {return id;}
+
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
+
+    public LocalDate getDate() {return date;}
+    public void setDate(LocalDate date) {this.date = date;}
+
+    public priority getPriorityTask() {return priorityTask;}
+    public void setPriorityTask(priority priorityTask) {this.priorityTask = priorityTask;}
+
+    public boolean isCompleted() {return completed;}
+    public void setCompleted(boolean completed) {this.completed = completed;}
+
+
+}
